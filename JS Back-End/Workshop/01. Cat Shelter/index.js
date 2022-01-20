c19c6c46f55af9ad376d91d8f3d27be6d6a5ab1a
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2022-01-20 18:13:52 +0200

    Insignificant changes made

--- JS Back-End/Workshop/01. Cat Shelter/index.js
+++ JS Back-End/Workshop/01. Cat Shelter/index.js
@@ -1,5 +1,39 @@
 const http = require(`http`);
+const fs = require(`fs`);
 
-const server = http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  switch (req.url) {
+    case "/":
+      let content = fs.readFileSync(`./views/home/index.html`);
+      res.writeHead(200, {
+        "Content-Type": "text/html",
+      });
+      res.write(content);
+      res.end();
+      break;
+
+    case "/styles/site.css":
+      let css = fs.readFileSync(`./styles/site.css`);
+      res.writeHead(200, {
+        "Content-Type": "text/css",
+      });
+      res.write(css);
+      res.end();
+      break;
+
+    case "/js/script.js":
+      let js = fs.readFileSync(`./js/script.js`);
+      res.writeHead(200, {
+        "Content-Type": "text/javascript",
+      });
+      res.write(js);
+      res.end();
+      break;
+
+    default:
+      res.statusCode = 404;
+      break;
+  }
+});
 
 server.listen(3000);
