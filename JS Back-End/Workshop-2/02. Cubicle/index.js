8308368dce0633745aefbcf8e3834ab6c5d5c34b
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2022-01-31 11:15:45 +0200

    Handlebars and express static used

--- JS Back-End/Workshop-2/02. Cubicle/index.js
+++ JS Back-End/Workshop-2/02. Cubicle/index.js
@@ -1,9 +1,23 @@
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || "development";
 
-const config = require('./config/config')[env];
-const app = require('express')();
+const config = require("./config/config")[env];
+const handlebars = require(`express-handlebars`);
+const express = require("express");
+const app = express();
 
-require('./config/express')(app);
-require('./config/routes')(app);
+require("./config/express")(app);
+require("./config/routes")(app);
 
-app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
+app.engine(`hbs`, handlebars({ extname: "hbs" }));
+app.set(`view engine`, `hbs`);
+app.use(express.static(`static`));
+
+app.get(`/`, (req, res) => {
+  res.render(`index`);
+});
+
+app.get(`*`, (req, res) => {
+  res.render(`404`);
+});
+
+app.listen(config.port, console.log(`Listening on port ${config.port}!`));
