5036ecbb6930009645abbcf36533dd791d101dc1
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2022-01-31 16:20:57 +0200

    Small changes made

--- JS Back-End/Workshop-2/02. Cubicle/config/routes.js
+++ JS Back-End/Workshop-2/02. Cubicle/config/routes.js
@@ -1,9 +1,12 @@
-const { Router } = require(`express`);
-const router = Router();
+const express = require(`express`);
+const router = express.Router();
 
 const cubeController = require(`../controllers/cubeController.js`);
 const homeController = require(`../controllers/homeController.js`);
 const errorController = require(`../controllers/errorController.js`);
+
+router.use(express.static(`static`));
+router.use(express.urlencoded());
 
 router.use(homeController);
 router.use(`/cube`, cubeController);
--- JS Back-End/Workshop-2/02. Cubicle/index.js
+++ JS Back-End/Workshop-2/02. Cubicle/index.js
@@ -5,11 +5,8 @@ const express = require("express");
 const app = express();
 
 const routes = require("./config/routes");
-
 require("./config/express")(app);
 
-app.use(express.static(`static`));
-app.use(express.urlencoded());
 app.use(routes);
 
 app.listen(config.port);
