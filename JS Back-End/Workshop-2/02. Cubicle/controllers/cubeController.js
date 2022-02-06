a0395fa97c162d074d0db60002b413f33c6fbfd8
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2022-02-06 11:53:38 +0200

    Added some new resources

--- JS Back-End/Workshop-2/02. Cubicle/controllers/cubeController.js
+++ JS Back-End/Workshop-2/02. Cubicle/controllers/cubeController.js
@@ -7,11 +7,15 @@ router.get(`/create`, (req, res) => {
   res.render(`create`);
 });
 
-router.post(`/create`, (req, res) => {
+router.post(`/create`, async (req, res) => {
   const { name, description, imageUrl, difficultyLevel } = req.body;
 
-  createCube(name, description, imageUrl, difficultyLevel);
-  res.redirect(`/`);
+  try {
+    await createCube(name, description, imageUrl, difficultyLevel);
+    res.redirect(`/`);
+  } catch (err) {
+    res.send(err.message);
+  }
 });
 
 router.get(`/:cubeId`, (req, res) => {
