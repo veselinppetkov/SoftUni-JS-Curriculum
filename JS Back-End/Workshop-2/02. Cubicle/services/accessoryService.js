18eac63114b980786b0b3bad9e114a011f504e13
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2022-02-06 12:24:22 +0200

    New service added

--- JS Back-End/Workshop-2/02. Cubicle/services/accessoryService.js
+++ JS Back-End/Workshop-2/02. Cubicle/services/accessoryService.js
@@ -1,38 +1,13 @@
-const Cube = require(`../models/Cube.js`);
+const Accessory = require(`../models/Accessory.js`);
 
-const getAllCubes = () => Cube.find({}).lean();
-
-const getCubeById = (id) => Cube.findById(id);
-
-const createCube = async (name, description, imageUrl, difficultyLevel) => {
-  await Cube.create({
+const createAccessory = async (name, description, imageUrl) => {
+  await Accessory.create({
     name,
     description,
     imageUrl,
-    difficultyLevel,
   });
 };
 
-const searchCube = (title, from, to) => {
-  let result = getAllCubes();
+const accessoryService = { createAccessory };
 
-  if (title) {
-    result = result.filter((x) =>
-      x.name.toLowerCase().includes(title.toLowerCase())
-    );
-  }
-
-  if (from) {
-    result = result.filter((x) => x.difficultyLevel >= from);
-  }
-
-  if (to) {
-    result = result.filter((x) => x.difficultyLevel <= to);
-  }
-
-  return result;
-};
-
-const cubeService = { createCube, getAllCubes, getCubeById, searchCube };
-
-module.exports = cubeService;
+module.exports = accessoryService;
