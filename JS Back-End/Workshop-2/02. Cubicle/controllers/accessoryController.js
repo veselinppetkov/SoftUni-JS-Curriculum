cc066861fb402401b0b702b5388b42dde96ba4ff
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2022-02-06 12:24:07 +0200

    Fixed accessoryController

--- JS Back-End/Workshop-2/02. Cubicle/controllers/accessoryController.js
+++ JS Back-End/Workshop-2/02. Cubicle/controllers/accessoryController.js
@@ -1,12 +1,21 @@
 const express = require(`express`);
 const router = express.Router();
 
+const { createAccessory } = require(`../services/accessoryService`);
+
 router.get(`/create`, (req, res) => {
   res.render(`accessory/create`);
 });
 
 router.post(`/create`, async (req, res) => {
-  const { name, description, imageUrl } = req.query;
+  const { name, description, imageUrl } = req.body;
+
+  try {
+    await createAccessory(name, description, imageUrl);
+    res.redirect(`/`);
+  } catch (err) {
+    res.send(err.message);
+  }
 });
 
 module.exports = router;
