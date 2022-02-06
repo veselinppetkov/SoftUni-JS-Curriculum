b1e186edb946cbb22a139fb87f5da21038fdd6f2
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2022-02-06 11:06:10 +0200

    Small changes made

--- JS Back-End/Workshop-2/02. Cubicle/models/Cube.js
+++ JS Back-End/Workshop-2/02. Cubicle/models/Cube.js
@@ -1,16 +1,4 @@
-const uniqid = require(`uniqid`);
-
 class Cube {
-  static #cubes = [
-    {
-      id: uniqid(),
-      name: "Ice Cube",
-      description: "A rapper",
-      imageUrl: "https://bit.ly/3IVdQxD",
-      difficultyLevel: "5",
-    },
-  ];
-
   constructor(name, description, imageUrl, difficultyLevel) {
     this.id = uniqid();
     this.name = name;
