b17449f3ccf9321667e7705efea3357e367dd052
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2022-02-15 16:10:46 +0200

    Created User Model

---
+++ JS Back-End/Workshop-2/02. Cubicle/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require(`mongoose`);
+const bcrypt = require(`bcrypt`);
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    minlength: [5, `Username must be above 5 symbols`],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: [4, `Password must be above 4 symbols`],
+  },
+});
+
+userSchema.pre(`save`, function (next) {
+  bcrypt.hash(this.password, 10).then((hash) => {
+    this.password = hash;
+    next();
+  });
+});
+
+const User = mongoose.model(`User`, userSchema);
+
+module.exports = User;
