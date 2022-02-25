0ebe7dc52dacfefc75d7e4a3dc79af363b4bb26b
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2022-02-25 10:14:06 +0200

    Implemented validation

--- JS Back-End/Workshop-2/02. Cubicle/models/User.js
+++ JS Back-End/Workshop-2/02. Cubicle/models/User.js
@@ -4,13 +4,22 @@ const bcrypt = require(`bcrypt`);
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
+    unique: true,
+    validate: [
+      /^[a-zA-Z0-9]+$/,
+      `You should use only English letters and digits`,
+    ],
     required: true,
-    minlength: [5, `Username must be above 5 symbols`],
+    minlength: [5, `Username should be above 5 symbols`],
   },
   password: {
     type: String,
+    validate: [
+      /^[a-zA-Z0-9]+$/,
+      `You should use only English letters and digits`,
+    ],
     required: true,
-    minlength: [4, `Password must be above 4 symbols`],
+    minlength: [8, `Password should be above 8 symbols`],
   },
 });
 
